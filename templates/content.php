10771df46673499133b7c16c382432a9cd53db56
Ana <agiraldow@gmail.com>
2015-09-16 20:00:19 -0400

    Start templating index page

--- templates/content.php
+++ templates/content.php
@@ -6,4 +6,36 @@
   <div class="entry-summary">
     <?php the_excerpt(); ?>
   </div>
+
+  <div class="brm-gallery">
+  	asdfsdf
+  </div>
+
+  <div class="brm-gallery production">
+
+  	<?php 
+  	if ( get_post_gallery() ) :
+  		$gallery = get_post_gallery( get_the_ID(), false );
+
+  	// $gallery_id_array = str_split( $gallery['ids'] );
+
+  	//   foreach( $gallery_id_array AS $id )
+  	//   {
+  	// 	$attachment_meta = wp_get_attachment( $id );
+
+  	// 	echo $attachment_meta;
+  	//   }
+
+  	/* Loop through all the images and output them one by one */
+  	foreach( $gallery['src'] AS $src )
+  	{
+  	?>
+
+  		<img src="<?php echo $src; ?>" class="my-custom-class" alt="Gallery image" />
+
+  	<?php
+  	}
+  	endif;
+  	?>
+  </div>
 </article>
