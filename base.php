034fcc69d137067419b093eac1ee036e37d970a3
Ana <agiraldow@gmail.com>
2015-09-16 21:20:24 -0400

    Add main styles

--- base.php
+++ base.php
@@ -20,7 +20,7 @@ use Roots\Sage\Wrapper;
     ?>
     <div class="wrap container" role="document">
       <div class="content row">
-        <main class="main" role="main">
+        <main class="main row" role="main">
           <?php include Wrapper\template_path(); ?>
         </main><!-- /.main -->
         <?php if (Config\display_sidebar()) : ?>
--- templates/content.php
+++ templates/content.php
@@ -1,21 +1,26 @@
-<article <?php post_class(); ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
-  </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
-  </div>
+<article <?php post_class('col-sm-8'); ?>>
+	<header>
+		<h2 class="entry-title"><?php the_title(); ?></h2>
+	</header>
+	<div class="entry-summary">
+		<?php the_excerpt(); ?>
+	</div>
 
-  <div class="brm-gallery">
-  	asdfsdf
-  </div>
+	<div class="brm-gallery">
+		<p class="image-description">
+			This is placement copy here for this image. Some copywriting will be here and this can go here ilke this and
+			maybe a link here and then here. This is placement copy here for this image. Some copywriting will be here and
+			this can go here ilke this and maybe a link here and then here.
 
-  <div class="brm-gallery production">
+		</p>
+		<img src="http://localhost:3000/the-brm/wp-content/uploads/2015/09/IMG_0167.jpg">
+	</div>
 
-  	<?php 
-  	if ( get_post_gallery() ) :
-  		$gallery = get_post_gallery( get_the_ID(), false );
+	<div class="brm-gallery production">
+
+		<?php 
+		if ( get_post_gallery() ) :
+			$gallery = get_post_gallery( get_the_ID(), false );
 
   	// $gallery_id_array = str_split( $gallery['ids'] );
 
@@ -26,16 +31,19 @@
   	// 	echo $attachment_meta;
   	//   }
 
-  	/* Loop through all the images and output them one by one */
-  	foreach( $gallery['src'] AS $src )
-  	{
-  	?>
+		/* Loop through all the images and output them one by one */
+		foreach( $gallery['src'] AS $src )
+		{
+			?>
 
-  		<img src="<?php echo $src; ?>" class="my-custom-class" alt="Gallery image" />
+			<img src="<?php echo $src; ?>" class="my-custom-class" alt="Gallery image" />
 
-  	<?php
-  	}
-  	endif;
-  	?>
-  </div>
+			<?php
+		}
+		endif;
+		?>
+	</div>
+	<h2 class="entry-title">Adidas</h2>
+	<h2 class="entry-title">Campaigns</h2>
+	<h2 class="entry-title">House of Marley</h2>
 </article>
